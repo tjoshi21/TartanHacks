c2a04a878bbeca23654451fff739d50ad215271d
NicholasSieger <nsieger1@gmail.com>
2017-02-10 21:36:54 -0500

    file structure

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+var config = {}
+
+config.cookieName = "COOKIE_NAME_HERE";
+config.cookieSecret = "COOKIE_SECRET_HERE";
+config.cookieParser = 'your secret here';
+
+config.admin_port = 4000;
+
+
+module.exports = config
---
+++ src/js/visualizer.js
@@ -0,0 +1,3 @@
+var w = 500,
+    h = 300;
+var svg = d3.select("body").select(".display").append("svg").attr({"width" : w,"height" : h});
