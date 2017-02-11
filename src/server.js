00430327e7d94a149d2b4b7b70757be0c1ce9c2a
NicholasSieger <nsieger1@gmail.com>
2017-02-10 23:39:37 -0500

    fixed file structure

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var http = require('http');
 
 var path = require('path');
-var cookieParser = require('cookie-parser');
+/* var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressSession = require('express-session');
 var errorHandler = require('express-error-handler');
+var expressSession = require('express-session'); */
 
 // var db = require('./lib/db')
 var config = require('./config');
@@ -13,29 +13,33 @@ var config = require('./config');
 //application
 var app = express();
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.set('view options', { layout: false });
 
-app.use(bodyParser.urlencoded({
+/*app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(bodyParser.json());
-app.use(cookieParser(config.cookieParser));
+ app.use(bodyParser.json());
+app.use(cookieParser(config.cookieParser)); */
 
 app.use('/public',express.static(__dirname + '/public'));
+/* app.use('/img',express.static(path.join(__dirname, 'public/images')));
+app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
+app.use('/css',express.static(path.join(__dirname, 'public/css'))); */
 GLOBAL.projdir = __dirname
 
-app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+//app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 
 // Register server routes
 ////////////////////////////////////////
 
-// require('./routes/admin')(app)
-// require('./routes/api')(app)
-// require('./routes/xhr')(app)
 app.get('/', function (req, res) {
-  	return res.render('gig_01.pug');
+  	return res.render('homepage.pug');
+});
+
+app.get('/login',function(req,res){
+    return res.render('login.pug');
 });
 
 
